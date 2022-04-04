c10b25a8624c83cc818936825efaa2c0e98b1ad8
aiotter <git@aiotter.com>
2022-04-04 13:50:03 +0900

    fix: Get config from environment variables if not defined at .env

--- config.ts
+++ config.ts
@@ -1,11 +1,11 @@
 import { config } from "https://deno.land/std@0.133.0/dotenv/mod.ts";
 
 const {
-  APPLICATION_ID,
-  PUBLIC_KEY,
-  TOKEN,
-  GUILD_ID,
-  THREADS_CATEGORY,
+  APPLICATION_ID = Deno.env.get("APPLICATION_ID")!,
+  PUBLIC_KEY = Deno.env.get("PUBLIC_KEY")!,
+  TOKEN = Deno.env.get("TOKEN")!,
+  GUILD_ID = Deno.env.get("GUILD_ID")!,
+  THREADS_CATEGORY = Deno.env.get("THREADS_CATEGORY")!,
 } = await config({ safe: true });
 
 export {
